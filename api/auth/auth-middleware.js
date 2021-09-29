d26b1ebbb41438b68ddc7840925fe92a47d457a7
Charlie May <61992595+charlie-may86@users.noreply.github.com>
2021-09-29 14:47:54 -0500

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,4 @@
-const User = require('../users/users-model')
+const User = require("../users/users-model");
 
 /*
   If the user does not have a session saved in the server
@@ -9,7 +9,7 @@ const User = require('../users/users-model')
   }
 */
 function restricted(req, res, next) {
-  console.log('this is the restricted middleware');
+  console.log("this is the restricted middleware");
   next();
 }
 
@@ -22,17 +22,16 @@ function restricted(req, res, next) {
   }
 */
 async function checkUsernameFree(req, res, next) {
-  try{
-    const users = await User.findBy({username: req.body.username})
-    if(!users.length){
-      next()
+  try {
+    const users = await User.findBy({ username: req.body.username });
+    if (!users.length) {
+      next();
     } else {
-      next({"message": "Username taken"})
+      next({ message: "Username taken", status: 422 });
     }
-  } catch(err){
-    next(err)
+  } catch (err) {
+    next(err);
   }
-  
 }
 
 /*
@@ -43,9 +42,17 @@ async function checkUsernameFree(req, res, next) {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists(req, res, next) {
-  console.log('this is the checkUsernameExists middleware');
-  next();
+async function checkUsernameExists(req, res, next) {
+  try {
+    const users = await User.findBy({ username: req.body.username });
+    if (!users.length) {
+      next();
+    } else {
+      next({ message: "Invalid credentials", status: 401 });
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
 /*
@@ -57,7 +64,7 @@ function checkUsernameExists(req, res, next) {
   }
 */
 function checkPasswordLength(req, res, next) {
-  console.log('this is the checkPasswordLength middleware');
+  console.log("this is the checkPasswordLength middleware");
   next();
 }
 
