a87cf998f97f0b86a3f6cbe37c2c97f93f31a3de
Charlie May <61992595+charlie-may86@users.noreply.github.com>
2021-09-29 14:49:44 -0500

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,4 @@
+const { OPEN_READWRITE } = require("sqlite3");
 const User = require("../users/users-model");
 
 /*
@@ -64,8 +65,11 @@ async function checkUsernameExists(req, res, next) {
   }
 */
 function checkPasswordLength(req, res, next) {
-  console.log("this is the checkPasswordLength middleware");
-  next();
+  if (!req.body.password || req.body.password.length < 3) {
+    next({ message: "Password must be longer than 3 chars", status: 422 });
+  } else {
+    next();
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
